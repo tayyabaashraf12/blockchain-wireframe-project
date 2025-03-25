c699fbbad0669c24bb67406a37e6e04a83197125
tayyabaashraf12 <tayyaba.ashraf@kryptomind.com>
2025-03-25 13:30:22 +0500

    commit first

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
